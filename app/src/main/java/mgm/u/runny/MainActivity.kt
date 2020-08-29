7cca72b30ed5465097be3a55c7a1997af9fbbe13
Moisés Gómez Medina <50937926+Nenva@users.noreply.github.com>
2020-08-29 11:32:45 -0700

    Dagger-Hilt Setup - Part 6

--- app/src/main/java/mgm/u/runny/MainActivity.kt
+++ app/src/main/java/mgm/u/runny/MainActivity.kt
@@ -2,10 +2,20 @@ package mgm.u.runny
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import dagger.hilt.android.AndroidEntryPoint
+import mgm.u.runny.db.RunDAO
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var runDAO: RunDAO
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("runDao", "RUNDAO: ${runDAO.hashCode()}")
     }
 }
---
+++ app/src/main/java/mgm/u/runny/di/AppModule.kt
@@ -0,0 +1,31 @@
+package mgm.u.runny.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import mgm.u.runny.db.RunningDatabase
+import mgm.u.runny.other.Constants.RUNNING_DATABASE_NAME
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideRunningDatabase(
+        @ApplicationContext app: Context
+    ) = Room.databaseBuilder(
+        app,
+        RunningDatabase::class.java,
+        RUNNING_DATABASE_NAME
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideRunDao(db: RunningDatabase) = db.getRunDao()
+}
